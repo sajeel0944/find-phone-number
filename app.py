3060126175930db7971e95a718c80c72c7a9c999
sajeek0944 <sajeel6y4z@gmail.com>
2025-02-21 21:48:53 +0500

    find phone number

---
+++ app.py
@@ -0,0 +1,34 @@
+import time
+import streamlit as st
+import phonenumbers
+from phonenumbers import geocoder
+
+
+st.header("📞Find Your Number")
+
+num : str = st.text_input("Inter Your Number (+)")
+
+
+if st.button("Find"):   # jab user click karry ga to ye chaly ga
+    
+    if num:    # agar num ky andar value hoye gi to ye chaly ga
+       
+        with st.spinner("⏳ Please wait..."):    # with 4 sec wait karry ga us ky baat hahat jaye ga
+            time.sleep(4)    # Simulate waiting
+
+        try:
+            phone_num = phonenumbers.parse(num) 
+            find_num = geocoder.description_for_number(phone_num, "en")
+
+            if find_num:   # agar find_num ky andar sahe number hoye ga to ye cahly ga wana nhi
+                st.markdown(f"<p>✔️ Successfully ! Find Your Number : <span style='color: red; font-size:20px; text-decoration: underline;'>{find_num}</span></p>", unsafe_allow_html=True) 
+            else :   # find_num ky andar sahe number nhi hoye ga to te cahly ga
+               st.markdown("<p style='color: red; font-size: 18px;'> ❌ Invalid number!</p>", unsafe_allow_html=True)
+               
+        except phonenumbers.NumberParseException:    # agar user ny num ky andar text dia to ye cahly ga
+            st.error("❌ Invalid phone number format! Please enter a valid number.")
+
+    else :    # agar user ny num ky andar kuch bhi nhi dia to ye cahly ga
+        st.warning("⚠️ Please enter a phone number before clicking Find.")
+
+
